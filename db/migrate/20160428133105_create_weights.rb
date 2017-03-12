33ed79da846534c5b488d6dbbbd89c4b83edb7f2
onigra <3280467rec@gmail.com>
2017-03-12 21:14:26 +0900

    migrationは5.0のバージョン指定

--- db/migrate/20160428133105_create_weights.rb
+++ db/migrate/20160428133105_create_weights.rb
@@ -1,4 +1,4 @@
-class CreateWeights < ActiveRecord::Migration
+class CreateWeights < ActiveRecord::Migration[5.0]
   def change
     create_table :weights do |t|
       t.decimal :weight, null: false, precision: 10, scale: 4
